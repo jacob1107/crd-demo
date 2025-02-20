44dd677cfb8e45ae19b1fb6b1ec67190957a4799
Jacob <jacob1107@163.com>
2025-02-20 17:27:43 +0800

    init

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1 contains API Schema definitions for the elasticweb v1 API group
-//+kubebuilder:object:generate=true
-//+groupName=elasticweb.com.bolingcavalry
+// +kubebuilder:object:generate=true
+// +groupName=elasticweb.com.bolingcavalry
 package v1
 
 import (
